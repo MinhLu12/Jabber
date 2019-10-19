f5c96ff4f04fdde4069386ea12181ebe21f568e6
Minh Lu <Minh.Lu45@T-mobile.com>
2019-10-18 22:08:34 -0700

    Modify CORS to only allow local domain

--- Jabber/app.py
+++ Jabber/app.py
@@ -1,38 +1,32 @@
-from flask import Flask, jsonify
-from pymongo import MongoClient
-from bson.json_util import dumps
-from flask_cors import CORS
+from flask import *
+from pymongo import *
+from bson.json_util import *
+from flask_cors import *
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r'/api/*': {"origins": "http://localhost:8080"}}, allow_headers='Content-Type')
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
 
+@app.route('/api/users', methods = ["GET"])
+def get_users():
+    #client = MongoClient('mongodb://localhost:27017/');
+    #mydb = client["test"];
 
-@app.route('/test', methods = ["GET"])
-def hello():
-    client = MongoClient('mongodb://localhost:27017/');
-    mydb = client["test"];
+    #posts = mydb.posts
+    ##post_data = {
+    ##    'title': 'Python and MongoDB',
+    ##    'content': 'PyMongo is fun, you guys',
+    ##    'author': 'Scott'
+    ##}
+    ##result = posts.insert_one(post_data);
 
-    posts = mydb.posts;
-    
-    #post_data = {
-    #    'title': 'Python and MongoDB',
-    #    'content': 'PyMongo is fun, you guys',
-    #    'author': 'Scott'
-    #}
-    #result = posts.insert_one(post_data);
+    #name = posts.find_one({'author': 'Scott'});
 
-    name = posts.find_one({'author': 'Scott'});
-
-    return dumps(name);
+    return jsonify("dfd");
 
 if __name__ == '__main__':
     import os
     HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT, debug = True)
+    app.run(HOST, port = 5000)
