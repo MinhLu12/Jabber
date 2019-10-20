ae8b275ee69f6c6b6770a68f69a02ae4512ffab1
Minh Lu <Minh.Lu45@T-mobile.com>
2019-10-19 19:26:16 -0700

    Include more data in the create user POST

--- Jabber/app.py
+++ Jabber/app.py
@@ -1,24 +1,33 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
-
 from repositories import user_repository
 
 app = Flask(__name__)
 api = Api(app)
+
 CORS(app, resources={r'/api/*': {"origins": "http://localhost:8080"}}, allow_headers='Content-Type')
+
+parser = reqparse.RequestParser()
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
 
-class Users(Resource):
+class UserCollection(Resource):
     def get(self):
         return user_repository.get_users()
 
-    def post(self):
-        return user_repository.create_user()
+api.add_resource(UserCollection, '/api/users')
 
-api.add_resource(Users, '/api/users')
+class User(Resource):
+    def post(self):
+        parser.add_argument('name', type = str, required = True)
+        parser.add_argument('age', type = int, required = True)
+        args = parser.parse_args()
+
+        return user_repository.create_user(args['name'], args['age'])
+
+api.add_resource(User, '/api/user')
 
 if __name__ == '__main__':
     import os
--- Jabber/repositories/user_repository.py
+++ Jabber/repositories/user_repository.py
@@ -1,9 +1,9 @@
 from bson.json_util import dumps
 from .database_singleton import db
 
-def create_user():
-    post_data = { 'name': 'minh' }
-    db().users.insert_one(post_data)
+def create_user(name, age):
+    created_user = db().users.insert_one({ 'name': name, 'age': age })
+    return dumps(created_user.inserted_id)
 
 def get_users():
     return dumps(db().users.find_one({'name': 'minh'}))
